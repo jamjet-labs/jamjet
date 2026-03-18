7e35c58379394d80ee757dc8d2735ce330c7f4f4
sdev <sunil@sunilprakash.com>
2026-03-19 00:32:21 +0530

    feat: add event timeline with type filtering and JSON expansion

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import { Shell } from '@/components/layout/Shell'
 import { WorkflowGraph } from '@/components/graph/WorkflowGraph'
 import { DetailSidebar } from '@/components/detail/DetailSidebar'
+import { EventTimeline } from '@/components/timeline/EventTimeline'
 
 export default function App() {
   return (
     <Shell
       graph={<WorkflowGraph />}
       detail={<DetailSidebar />}
-      timeline={<div className="p-4 text-zinc-600 text-sm">Event timeline</div>}
+      timeline={<EventTimeline />}
     />
   )
 }
