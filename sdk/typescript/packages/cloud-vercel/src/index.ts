e8f11672c45c46d31f40b72478bbf21291bcdacd
sdev <sunil@sunilprakash.com>
2026-05-09 12:13:13 +0530

    feat(ts-vercel): re-export public API

--- sdk/typescript/packages/cloud-vercel/src/index.ts
+++ sdk/typescript/packages/cloud-vercel/src/index.ts
@@ -1,1 +1,7 @@
 export const VERSION = '0.1.0'
+
+export { jamjetMiddleware } from './middleware.js'
+export type { JamjetMiddlewareOptions } from './middleware.js'
+
+export { registerJamjetTelemetry } from './telemetry.js'
+export type { RegisterTelemetryOptions } from './telemetry.js'
